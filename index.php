6d562333a839b74d32bf53de6e2ba1ceaa2fccca
SLIMANI Kaïna <slimani.kaina@gmail.com>
2026-03-30 18:24:11 +0200

    Updated the file to manage the addition of legal pages

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
-error_reporting(E_ALL & ~E_DEPRECATED);
-ini_set("display_errors", 1);
+// protection par silence, pas le droit de lister ce répertoire : va voir en haut...
 
-define("CHARGE_AUTOLOAD",true);
-require_once("inc/poo.inc.php"); 
-
-$page = $_GET["page"] ?? "";
-
-switch($page) {
-    case "login":
-        $view = new ViewLogin();
-        echo $view;
-        break;
-    default:
-        $view = new ViewLandingPage();
-        echo $view;
-        break;
-}
-
+header('Location: ..');
 ?>
