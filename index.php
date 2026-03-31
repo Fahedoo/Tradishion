107a2f7380751e4b005d73fe1b2a072a5088f21c
SLIMANI Kaïna <slimani.kaina@gmail.com>
2026-03-31 16:24:36 +0200

    Updated the file to manage the addition of legal pages
    File transmission error yesterday

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
-// protection par silence, pas le droit de lister ce répertoire : va voir en haut...
+error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set("display_errors", 1);
 
-header('Location: ..');
+define("CHARGE_AUTOLOAD",true);
+require_once("inc/poo.inc.php"); 
+
+$page = $_GET["page"] ?? "landing";
+
+switch($page) {
+    case "login":
+        $view = new ViewLogin();
+        echo $view;
+        break;
+    case "signup":
+        $view = new ViewSignUp();
+        echo $view;
+        break;
+    case "legal":
+        $view = new ViewLegal();
+        echo $view;
+        break;
+    case "privacy":
+        $view = new ViewPrivacy();
+        echo $view;
+        break;
+    case "gcu":
+        $view = new ViewCGU();
+        echo $view;
+        break;
+    default:
+        $view = new ViewLandingPage();
+        echo $view;
+        break;
+}
+
 ?>
